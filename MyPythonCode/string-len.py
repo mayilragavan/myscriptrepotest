f94863850dd36527dd0705eb60b810bbf1a9bf51
mayilragavan <146420395+mayilragavan@users.noreply.github.com>
2025-07-12 05:25:41 +0000

    MyLearnging

---
+++ MyPythonCode/string-len.py
@@ -0,0 +1,5 @@
+mytext = "This is mayilLearning Python"
+
+length = len(mytext)
+
+print("Length of the string is:", length )
---
+++ MyPythonCode/string-replace.py
@@ -0,0 +1,3 @@
+text = "Mayil is learning Python Program"
+new_text = text.replace("Python", "PowerShell")
+print("Modified test is:", new_text)
